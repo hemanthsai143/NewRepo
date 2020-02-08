5eb2943af1ad561fc738b669ac3aa29ff568d84b
Hemanth <hemanthsaibommuluri35@gmail.com>
2020-02-08 16:34:05 +0530

    My first commit

--- src/com/printnum/PrintNumbers.java
+++ src/com/printnum/PrintNumbers.java
@@ -3,12 +3,27 @@ package com.printnum;
 public class PrintNumbers {
 
 	public static void main(String[] args) {
+		String primeNumber=" ";
 	
-		for(int i=1;i<=100;i++)
+		for(int i=1;i<=1000;i++)
 		{
-			System.out.println(i);
+			int counter=0;
+			for(int j=2;j<=i-1;j++)
+			{
+			
+				if(i%j==0)
+				{
+					counter++;
+				}
+					
 		}
-
+         if(counter==0)
+         {
+        	 primeNumber=primeNumber+i+" ";
+         }
+         
 	}
+		System.out.println(primeNumber);
 
+}
 }
